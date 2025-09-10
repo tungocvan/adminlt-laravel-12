4e3950da45e7351aa9c1dc11305924a1117405b8
Tu Ngoc Van <tungocvan@gmail.com>
2025-09-10 14:00:36 +0700

    10092025

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -3,9 +3,76 @@
 namespace App\Livewire\Auth;
 
 use Livewire\Component;
+use Livewire\Attributes\Validate;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
 
 class Login extends Component
 {
+    
+    
+    public $email;   
+    
+    public $password;
+    
+    protected $rules = [
+        'email'    => 'required',
+        'password' => 'required|min:6',
+    ];
+    
+    protected $messages = [
+        'email.required'    => 'Tài khoản đăng nhập không được bỏ trống',        
+        'password.required' => 'Mật khẩu không được bỏ trống',
+        'password.min'      => 'Mật khẩu phải có ít nhất 6 ký tự',
+    ];
+
+    public $remember=false;
+
+    public function mount()
+    {
+        if (Auth::check()) {
+            return redirect()->to('/');
+        }
+    }
+
+
+    public function save()
+    {
+        $this->validate();
+        $username = $this->email;
+        $password = $this->password;
+       
+        if (filter_var($username, FILTER_VALIDATE_EMAIL)) {
+            $user = \App\Models\User::where('email', $username)->first();
+            
+            if (!$user) {
+                throw ValidationException::withMessages([
+                    'email' => 'Email không tồn tại trong hệ thống.',
+                ]);
+            }
+
+            // Kiểm tra email_verified_at
+            if (is_null($user->email_verified_at) && $user->is_admin != 1) {
+                throw ValidationException::withMessages([
+                    'email' => 'Tài khoản chưa được admin phê duyệt.',
+                ]);
+            }
+
+
+            $username = $user->username;
+        }
+
+
+        if (!Auth::attempt(['username' => $username, 'password' => $password], $this->remember)) {
+            throw ValidationException::withMessages([
+                'email' => 'Thông tin đăng nhập không đúng.',
+            ]);
+        }
+
+        return redirect()->intended('/'); // hoặc route bạn muốn
+    }
+
     public function render()
     {
         return view('livewire.auth.login');
--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -8,27 +8,30 @@
           <div class="card-body">
             <p class="login-box-msg">Sign in to start your session</p>
       
-            <form action="../../index3.html" method="post">
+            <form wire:submit="save">
               <div class="input-group mb-3">
-                <input type="email" class="form-control" placeholder="Email">
+                <input type="text" class="form-control" placeholder="Email or Username" wire:model="email">
                 <div class="input-group-append">
                   <div class="input-group-text">
                     <span class="fas fa-envelope"></span>
                   </div>
                 </div>
+                @error('email') <span class="error w-100" style="color:red;font-size:12px"> {{ $message }}</span> @enderror
+                
               </div>
               <div class="input-group mb-3">
-                <input type="password" class="form-control" placeholder="Password">
+                <input type="password" class="form-control" placeholder="Password" wire:model="password">
                 <div class="input-group-append">
                   <div class="input-group-text">
                     <span class="fas fa-lock"></span>
                   </div>
                 </div>
+                @error('password') <span class="error w-100" style="color:red;font-size:12px"> {{ $message }}</span> @enderror
               </div>
               <div class="row">
                 <div class="col-8">
                   <div class="icheck-primary">
-                    <input type="checkbox" id="remember">
+                    <input type="checkbox" id="remember" wire:model="remember">
                     <label for="remember">
                       Remember Me
                     </label>
