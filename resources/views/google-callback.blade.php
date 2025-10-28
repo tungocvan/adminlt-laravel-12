133d4abe23b855f7d6e41b8e3fa3a245ba56dda6
Tu Ngoc Van <tungocvan@gmail.com>
2025-10-28 15:24:36 +0700

    28102025-4

--- resources/views/google-callback.blade.php
+++ resources/views/google-callback.blade.php
@@ -1,17 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Google OAuth Callback</title>
+</head>
+<body>
 <script>
-  // Lấy token từ fragment
-  const hash = window.location.hash.substr(1); // bỏ #
+  // 1️⃣ Lấy access_token từ fragment
+  const hash = window.location.hash.substring(1); // bỏ #
   const params = Object.fromEntries(new URLSearchParams(hash));
   const token = params.access_token;
 
-  // Gửi token lên backend
-  fetch('https://adminlt.tungocvan.com/api/google/callback', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ id_token: token })
-  }).then(res => res.json())
-    .then(data => {
-      // chuyển hướng về app / dashboard
-      window.location = '/'; 
-    });
+  // 2️⃣ Gửi token về React Native bằng postMessage
+  if (window.ReactNativeWebView) {
+    // nếu chạy trong WebView, gửi về RN
+    window.ReactNativeWebView.postMessage(JSON.stringify({ token }));
+  } else {
+    // Nếu dùng Expo WebBrowser.openAuthSessionAsync
+    // chrome custom tabs / safari view controller sẽ detect URL scheme
+    const urlScheme = new URLSearchParams({ token });
+    window.location = `myapp://oauth2redirect?${urlScheme.toString()}`;
+  }
 </script>
+</body>
+</html>
